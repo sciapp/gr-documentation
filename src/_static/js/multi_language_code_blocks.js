a7f67c42d9f101a8fdc53b0625d02558c87d7645
Florian Rhiem <f.rhiem@fz-juelich.de>
2018-01-05 16:01:37 +0100

    Cleaned up src/_static/js/multi_language_code_blocks.js

--- src/_static/js/multi_language_code_blocks.js
+++ src/_static/js/multi_language_code_blocks.js
@@ -1,32 +1,32 @@
 var known_languages = [
-	{id: "python", name: "Python"},
-	{id: "c", name: "C"},
-	{id: "julia", name: "Julia"}
+  {id: "python", name: "Python"},
+  {id: "c", name: "C"},
+  {id: "julia", name: "Julia"}
 ];
 
 function setLanguage(language) {
-	$(".highlight").each(function() {
-	  var highlighted_codeblock = $(this.parentNode);
-	  $.each(known_languages, function() {
-	    var known_language = this;
-			if (highlighted_codeblock.hasClass("highlight-"+this.id)) {
-				if (this == language) {
-					highlighted_codeblock.show();
-				} else {
-					highlighted_codeblock.hide();
-				}
-			}
+  $(".highlight").each(function() {
+    var highlighted_codeblock = $(this.parentNode);
+    $.each(known_languages, function() {
+      var known_language = this;
+      if (highlighted_codeblock.hasClass("highlight-"+this.id)) {
+        if (this == language) {
+          highlighted_codeblock.show();
+        } else {
+          highlighted_codeblock.hide();
+        }
+      }
     });
   });
 
-	$(".language-picker-item").each(function() {
-		var language_picker_item = $(this);
-		if (language_picker_item.hasClass("language-picker-item-"+language.id)) {
-			language_picker_item.addClass("language-picker-item-active");
-		} else {
-			language_picker_item.removeClass("language-picker-item-active");
-		}
-	});
+  $(".language-picker-item").each(function() {
+    var language_picker_item = $(this);
+    if (language_picker_item.hasClass("language-picker-item-"+language.id)) {
+      language_picker_item.addClass("language-picker-item-active");
+    } else {
+      language_picker_item.removeClass("language-picker-item-active");
+    }
+  });
 }
 
 function addLanguagePickers() {
@@ -43,103 +43,56 @@ function addLanguagePickers() {
     return;
   }
 
-	var highlighted_codeblocks = $(".highlight").filter(function() {
-	  for (var i = 0; i < known_languages.length; i++) {
-	    if ($(this.parentNode).hasClass("highlight-"+known_languages[i].id)) {
-	      return true;
+  var highlighted_codeblocks = $(".highlight").filter(function() {
+    for (var i = 0; i < known_languages.length; i++) {
+      if ($(this.parentNode).hasClass("highlight-"+known_languages[i].id)) {
+        return true;
       }
     }
-	  return false;
-	});
-	var highlighted_codeblocks_versions = [];
-	for (var i = 0; i < highlighted_codeblocks.length; i++) {
-		var highlighted_codeblock = $(highlighted_codeblocks[i].parentNode);
+    return false;
+  });
+  var highlighted_codeblocks_versions = [];
+  for (var i = 0; i < highlighted_codeblocks.length; i++) {
+    var highlighted_codeblock = $(highlighted_codeblocks[i].parentNode);
 
-		if (!highlighted_codeblock.prev() || highlighted_codeblock.prev().find(".highlight").length == 0) {
-			// Reset list of different language versions of the same code
-			var highlighted_codeblocks_versions = [];
-		}
-		highlighted_codeblocks_versions.push(highlighted_codeblock);
-		if (!highlighted_codeblock.next() || highlighted_codeblock.next().find(".highlight").length == 0) {
-			var language_picker = document.createElement("ul");
-			language_picker.className = "language-picker";
-			for (var j = 0; j < known_languages.length; j++) {
-				var language = known_languages[j];
-				var has_version_in_language = false;
-				for (var k = 0; k < highlighted_codeblocks_versions.length; k++) {
-					if (jQuery(highlighted_codeblocks_versions[k]).hasClass("highlight-"+language.id)) {
-						has_version_in_language = true;
-					}
-				}
-				if (!has_version_in_language) {
-				  var placeholder = document.createElement("div");
-				  placeholder.className = "highlight-placeholder highlight-"+language.id;
-				  var placeholder_content = document.createElement("div");
-				  placeholder_content.className = "highlight";
-				  placeholder.appendChild(placeholder_content);
-			    $(placeholder).insertBefore(highlighted_codeblock);
+    if (!highlighted_codeblock.prev() || highlighted_codeblock.prev().find(".highlight").length == 0) {
+      // Reset list of different language versions of the same code
+      var highlighted_codeblocks_versions = [];
+    }
+    highlighted_codeblocks_versions.push(highlighted_codeblock);
+    if (!highlighted_codeblock.next() || highlighted_codeblock.next().find(".highlight").length == 0) {
+      var language_picker = document.createElement("ul");
+      language_picker.className = "language-picker";
+      for (var j = 0; j < known_languages.length; j++) {
+        var language = known_languages[j];
+        var has_version_in_language = false;
+        for (var k = 0; k < highlighted_codeblocks_versions.length; k++) {
+          if (jQuery(highlighted_codeblocks_versions[k]).hasClass("highlight-"+language.id)) {
+            has_version_in_language = true;
+          }
         }
-				var language_picker_item = document.createElement("li");
-				var label = document.createTextNode(language.name);
-				language_picker_item.appendChild(label);
-				language_picker_item.className = "language-picker-item language-picker-item-"+language.id;
-				if (has_version_in_language) {
-					language_picker_item.addEventListener("click", function() {
-						setLanguage(this);
-					}.bind(language));
-				} else {
-					language_picker_item.className += " language-picker-item-disabled";
-				}
-				language_picker.appendChild(language_picker_item);
-			}
-			$(language_picker).insertAfter(highlighted_codeblock);
-		}
-	}
-
-
-
-  //
-	// var highlighted_codeblocks = document.getElementsByClassName("highlight");
-	// var highlighted_codeblocks_versions = [];
-	// for (var i = 0; i < highlighted_codeblocks.length; i++) {
-	// 	var highlighted_codeblock = highlighted_codeblocks[i].parentNode;
-	// 	if (!highlighted_codeblock.previousElementSibling || highlighted_codeblock.previousElementSibling.getElementsByClassName("highlight").length == 0) {
-	// 		// Reset list of different language versions of the same code
-	// 		var highlighted_codeblocks_versions = [];
-	// 	}
-	// 	highlighted_codeblocks_versions.push(highlighted_codeblock);
-	// 	if (!highlighted_codeblock.nextElementSibling || highlighted_codeblock.nextElementSibling.getElementsByClassName("highlight").length == 0) {
-	// 		var language_picker = document.createElement("ul");
-	// 		language_picker.className = "language-picker";
-	// 		for (var j = 0; j < known_languages.length; j++) {
-	// 			var language = known_languages[j];
-	// 			var has_version_in_language = false;
-	// 			for (var k = 0; k < highlighted_codeblocks_versions.length; k++) {
-	// 				if (jQuery(highlighted_codeblocks_versions[k]).hasClass("highlight-"+language.id)) {
-	// 					has_version_in_language = true;
-	// 				}
-	// 			}
-	// 			var language_picker_item = document.createElement("li");
-	// 			var label = document.createTextNode(language.name);
-	// 			language_picker_item.appendChild(label);
-	// 			language_picker_item.className = "language-picker-item language-picker-item-"+language.id;
-	// 			if (has_version_in_language) {
-	// 				language_picker_item.addEventListener("click", function() {
-	// 					setLanguage(this);
-	// 				}.bind(language));
-	// 			} else {
-	// 				language_picker_item.className += " language-picker-item-disabled";
-	// 			}
-	// 			language_picker.appendChild(language_picker_item);
-	// 		}
-	// 		highlighted_codeblock.parentNode.insertBefore(language_picker, highlighted_codeblock.nextSibling);
-	// 	}
-	// }
+        var language_picker_item = document.createElement("li");
+        var label = document.createTextNode(language.name);
+        language_picker_item.appendChild(label);
+        language_picker_item.className = "language-picker-item language-picker-item-"+language.id;
+        if (has_version_in_language) {
+          language_picker_item.addEventListener("click", function() {
+            setLanguage(this);
+          }.bind(language));
+        } else {
+          language_picker_item.className += " language-picker-item-disabled";
+          $(placeholder).before("<div class='highlight-placeholder highlight-"+language.id+"'><div class='highlight'></div></div>");
+        }
+        language_picker.appendChild(language_picker_item);
+      }
+      $(language_picker).insertAfter(highlighted_codeblock);
+    }
+  }
 }
 
 $(function () {
-	addLanguagePickers();
-	if (known_languages.length > 0) {
+  addLanguagePickers();
+  if (known_languages.length > 0) {
     setLanguage(known_languages[0]);
   }
 });
